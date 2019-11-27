c5aac9f8516e4d2755c8d79e22e496d9e187c392
Seventty <elmastigeron2015@gmail.com>
2019-11-27 03:49:20 -0400

    Library creation.

---
+++ Control.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SkyPixCH
+{
+    public class Control
+    {
+        public static DataSet Run(string cmd)
+        {
+            SqlConnection SkyCon = new SqlConnection("Data Source=.;Initial Catalog=SkyPix;Integrated Security=True");
+            SkyCon.Open();
+            DataSet Dt = new DataSet();
+            SqlDataAdapter SDA = new SqlDataAdapter(cmd, SkyCon);
+            SDA.Fill(Dt);
+            SkyCon.Close();
+            return Dt;
+        }
+    }
+}
+
